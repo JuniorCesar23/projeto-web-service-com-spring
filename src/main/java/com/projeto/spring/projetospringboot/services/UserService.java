5f7a57bf3d9255f1050c2992806f25bf7f96539f
Júnior César <cesarck36@gmail.com>
2021-06-09 15:04:09 -0300

    Exception handling - update

--- src/main/java/com/projeto/spring/projetospringboot/services/UserService.java
+++ src/main/java/com/projeto/spring/projetospringboot/services/UserService.java
@@ -13,41 +13,47 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
     }
 
-    public User findbyId(Long id){
+    public User findbyId(Long id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ResourcesNotAfoundException(id));
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return userRepository.save(obj);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         try {
             userRepository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
             throw new ResourcesNotAfoundException(id);
-        } catch (DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(e.getMessage());
         }
     }
-    
 
-    public User update(Long id, User obj){
-        User entity = userRepository.getOne(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+    public User update(Long id, User obj) {
+        try {
+            User entity = userRepository.getOne(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourcesNotAfoundException(e.getMessage());
+        }
     }
+
     private void updateData(User entity, User obj) {
         entity.setNome(obj.getNome());
         entity.setEmail(obj.getEmail());
